e864a7f69cabdfe31fd210bab8189577cc28856e
isadora <isadoravanco@gmail.com>
2024-02-16 16:30:32 -0300

    feat: Atividades 02

--- Exercicios/4Semestre/Atividade02/atividade02-intercala.asm
+++ Exercicios/4Semestre/Atividade02/atividade02-intercala.asm
@@ -7,31 +7,39 @@
 .data
 ent1: .asciiz "Insira a string 1: "
 ent2: .asciiz "Insira a string 2: "
+resultado: .asciiz "=>Resultado: "
+
+.align 2
 str1: .space 100
 str2: .space 100
 str3: .space 200
 
 .text
+.globl main 
 main:
-    la $a0, ent1    # Parâmetro: mensagem
-    la $a1, str1    # Parâmetro: endereço da string
-    jal leitura     # Leitura (mensagem, string)
+    la $a0, ent1        # Parâmetro: mensagem
+    la $a1, str1        # Parâmetro: endereço da string
+    jal leitura         # Leitura (mensagem, string)
 
-    la $a0, ent2    # Parâmetro: mensagem
-    la $a1, str2    # Parâmetro: endereço da string
-    jal leitura     # Leitura (mensagem, string)
+    la $a0, ent2        # Parâmetro: mensagem
+    la $a1, str2        # Parâmetro: endereço da string
+    jal leitura         # Leitura (mensagem, string)
 
-    la $a0, str1    # Parâmetro: endereço da string 1
-    la $a1, str2    # Parâmetro: endereço da string 2
-    la $a2, str3    # Parâmetro: endereço da string 3
-    jal intercala   # Intercala (str1, str2, str3)
+    la $a0, str1        # Parâmetro: endereço da string 1
+    la $a1, str2        # Parâmetro: endereço da string 2
+    la $a2, str3        # Parâmetro: endereço da string 3
+    jal intercala       # Intercala (str1, str2, str3)
 
-    move $a0, $v0   # Move o retorno da string resultante
-    li $v0, 4       # Código de impressão de string
-    syscall         # Imprime a string intercalada
+    la $a0, resultado # Move o retorno da string resultante
+    li $v0, 4           # Código de impressão de string
+    syscall             # Imprime a string de resultado
 
-    li $v0, 10      # Código para finalizar o programa 
-    syscall         # Finaliza o programa
+    la $a0, str3      # Move o retorno da string resultante
+    li $v0, 4           # Código de impressão de string
+    syscall             # Imprime a string intercalada
+
+    li $v0, 10          # Código para finalizar o programa 
+    syscall             # Finaliza o programa
 
 leitura:
     li $v0, 4       # Código para impressão de string
@@ -50,6 +58,10 @@ intercala:
     li $s0, 0
     # $s1 = '\n'
     li $s1, 10
+    # $s2 = &string3[i]
+    move $s2, $a2
+    # $s3 = &string3
+    move $s3, $a2
 
     # $t0 = i
     li $t0, 0
@@ -64,14 +76,14 @@ intercala:
 
     lIntercala:
         # $t5 = string1[str1]
-        lw $t5, ($t3)
+        lb $t5, ($t3)              
 
-        beq $t5, 0, fimStr1         # if(string1[str1] == '\0'): goto fimStr1
-        beq $t5, 10, fimStr1        # if(string1[str1] == '\n'): goto fimStr1
+        beq $t5, $s0, fimStr1         # if(string1[str1] == '\0'): goto fimStr1
+        beq $t5, $s1, fimStr1        # if(string1[str1] == '\n'): goto fimStr1
 
-        sw $t5, ($a2)               # string3[i] = string1[str1]
+        sb $t5, ($s2)               # string3[i] = string1[str1]
         addi $t3, $t3, 1            # &string1[str1 + 1]
-        addi $a2, $a2, 1            # &string3[i + 1]
+        addi $s2, $s2, 1            # &string3[i + 1]
         addi $t0, $t0, 1            # i++
 
         j verificaStr2
@@ -81,36 +93,36 @@ intercala:
 
         verificaStr2:
             # $t5 = string2[str2]
-            lw $t5, ($t4)
+            lb $t5, ($t4)
 
-            beq $t5, 0, fimStr2         # if(string2[str2] == '\0'): goto fimStr2
-            beq $t5, 10, fimStr2        # if(string2[str2] == '\n'): goto fimStr2
+            beq $t5, $s0, fimStr2         # if(string2[str2] == '\0'): goto fimStr2
+            beq $t5, $s1, fimStr2        # if(string2[str2] == '\n'): goto fimStr2
 
-            sw $t5, ($a2)               # string3[i] = string2[str2]
+            sb $t5, ($s2)               # string3[i] = string2[str2]
             addi $t4, $t4, 1            # &string2[str1 + 1]
-            addi $a2, $a2, 1            # &string3[i + 1]
+            addi $s2, $s2, 1            # &string3[i + 1]
             addi $t0, $t0, 1            # i++
 
             j verificaFim
 
             fimStr2:
-                addi $t1, $zero, -1     # str1 = -1
+                addi $t2, $zero, -1     # str2 = -1
 
         verificaFim:
             blt $t0, 200, verificaFim2
             j finaliza
 
         verificaFim2:
-            beq $t1, -1, verificaFim2   # if(str1 == -1): goto verificaFim2
+            beq $t1, -1, verificaFim3   # if(str1 == -1): goto verificaFim3
             j lIntercala
         
         verificaFim3:
-            beq $t2, -1, finaliza   # if(str2 == -1): goto verificaFim3
+            beq $t2, -1, finaliza       # if(str2 == -1): goto finaliza
             j lIntercala
 
         finaliza:
-            sw $s0, ($a2)               # string3[i] = '\0'
-            sub $a2, $a2, $t0
-            move $v0, $a2           # $v0 = &string3
+            sb $s0, ($s2)               # string3[i] = '\0'
+            sub $s2, $s2, $t0
+            move $v0, $s3               # $v0 = &string3
 
             jr $ra
---
+++ Exercicios/4Semestre/Atividade02/atividade02-palindromo.asm
@@ -0,0 +1,130 @@
+# 2. Elaborar um programa, em código MIPS, que faça a leitura 
+# de uma string ASCII e verifique se a mesma é um palíndromo 
+# (retorne 1 se for palíndromo e 0 se não for palíndromo).  
+
+# Aluna: Isadora Vanço
+.data
+ent1: .asciiz "Insira a string: "
+resultado: .asciiz "=>Resultado: "
+strEhPalindromo: .asciiz "É palindromo\n"
+strNaoEhPalindromo: .asciiz "Não é palindromo\n"
+
+.align 2
+str: .space 200
+
+.text 
+.globl main 
+main:
+    la $a0, ent1        # Parâmetro: mensagem
+    la $a1, str         # Parâmetro: endereço da string
+    jal leitura         # Leitura (mensagem, string)
+
+    la $a0, resultado   # Move o retorno da string resultante
+    li $v0, 4           # Código de impressão de string
+    syscall             # Imprime a string intercalada
+
+    # Retorna 1 se for palindromo, se não, retorna 0
+    la $a0, str
+    jal verificaPalindromo
+
+    bne $v0, 1, naoPalindromo   # if($v0 != 1): goto naoPalindromo
+    
+    la $a0, strEhPalindromo # Move o retorno da string resultante
+    li $v0, 4           # Código de impressão de string
+    syscall             # Imprime a string intercalada
+    
+    j fim
+
+    naoPalindromo:
+        la $a0, strNaoEhPalindromo  # Move o retorno da string resultante
+        li $v0, 4           # Código de impressão de string
+        syscall             # Imprime a string intercalada
+
+    fim:
+        li $v0, 10          # Código para finalizar o programa 
+        syscall             # Finaliza o programa
+
+leitura:
+    li $v0, 4       # Código para impressão de string
+    syscall         # Imprime a string
+    
+    move $a0, $a1   # Endereço da string para leitura
+    li $a1, 100     # Número máximo de caracteres
+    
+    li $v0, 8       # Código de leitura da string
+    syscall         # Faz a leitura da string
+
+    jr $ra          # Retorna para a main
+
+verificaPalindromo:
+    # $s0 = '\0'
+    li $s0, 0
+    # $s1 = '\n'
+    li $s1, 10
+    # $s2 = &string
+    move $s2, $a0
+
+    # $t0 = i
+    li $t0, 0
+    # $t1 = f
+    li $t1, 0
+    # $t2 = &string[i] = Primeiros caracteres
+    move $t2, $a0
+    # $t4 = &string[f] = Ultimos caracteres
+    move $t3, $a0
+
+    # Se a string é apenas '\n' ou '\0' (Acho que não precisa, mas por garantia...)
+    # $t4 = string[f]
+    lb $t4, ($t3)
+    beq $t4, $s0, verificaExtremos  # if(string[f] == '\0'): goto verificaExtremos
+    beq $t4, $s1, verificaExtremos  # if(string[f] == '\n'): goto verificaExtremos
+
+    # Procura pelo último caractere
+    procuraUltimo:
+        # $t4 = &string[f + 1]
+        addi $t4, $t3, 1
+
+        # $t5 = string[f + 1]
+        lb $t5, ($t4)
+
+        # Se string[f] já é o último caractere
+        beq $t5, $s0, verificaExtremos  # if(string[f + 1] == '\0'): goto verificaExtremos
+        beq $t5, $s1, verificaExtremos  # if(string[f + 1] == '\n'): goto verificaExtremos
+
+        # f++
+        addi $t1, $t1, 1
+        addi $t3, $t3, 1    
+
+        j procuraUltimo
+    
+    # Verifica os extremos
+    verificaExtremos:
+        # Chegou no meio da string sem erros
+        bge $t0, $t1, retornaPalindromo # if(i >= f): goto retornaPalindromo
+
+        # $t4 = string[i]
+        lb $t4, ($t2)
+        # $t5 = string[f]
+        lb $t5, ($t3)
+
+        bne $t4, $t5, retornaNaoPalindromo  # if(string[i] != string[f]): goto retornaNaoPalindromo
+
+        # i++
+        addi $t0, $t0, 1    
+        addi $t2, $t2, 1
+
+        # f--
+        subi $t1, $t1, 1    
+        subi $t3, $t3, 1
+
+        j verificaExtremos
+
+    retornaNaoPalindromo:
+        li $v0, 0            # $v0 = 0
+
+        jr $ra
+
+    retornaPalindromo:
+        li $v0, 1            # $v0 = 1
+
+        jr $ra
