836717d568951c09d91e40cea491c88aa1c677dc
isadora <isadoravanco@gmail.com>
2024-03-29 23:58:42 -0300

    feat: Exemplos de ponto flutuante

---
+++ Exemplos/converteInteiro.asm
@@ -0,0 +1,55 @@
+.data
+intPi: .word -1   
+pi: .double 3.1415
+strValorA: .asciiz "\n=> Valor de A (inteiro): "
+strValorB: .asciiz "\n=> Valor de B (double): "
+
+.text
+.globl main
+main:
+
+# Valores antes da conversão
+la $a0, strValorA       # Carrega o endereço da string
+li $v0, 4               # Código de impressão de string
+syscall                 # Imprime a string
+
+lw $a0, intPi           # $a0 = *intPi
+li $v0, 1               # Código para imprimir inteiro
+syscall
+
+la $a0, strValorB       # Carrega o endereço da string
+li $v0, 4               # Código de impressão de string
+syscall                 # Imprime a string
+
+l.d $f12, pi            # $f12 = *pi
+li $v0, 3               # Código para imprimir float double
+syscall
+
+# Faz a conversão
+# l.d (load double) => carrega o valor double do endereço
+l.d $f10, pi
+# cvt.w.d (convert word double) => converte o valor inteiro para double
+cvt.w.d $f12, $f10
+# mtcI (move from coprocessor 1) => copia o conteúdo do coprocessador FPU no registrador destino
+mfc1 $t1, $f12
+sw $t1, intPi
+
+# Valores depois da conversão
+la $a0, strValorA       # Carrega o endereço da string
+li $v0, 4               # Código de impressão de string
+syscall                 # Imprime a string
+
+lw $a0, intPi           # $a0 = *intPi
+li $v0, 1               # Código para imprimir inteiro
+syscall
+
+la $a0, strValorB       # Carrega o endereço da string
+li $v0, 4               # Código de impressão de string
+syscall                 # Imprime a string
+
+l.d $f12, pi            # $f12 = *pi
+li $v0, 3               # Código para imprimir float double
+syscall
+
+li $v0, 10
+syscall
